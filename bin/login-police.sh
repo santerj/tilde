2bb62a8bf81bb3b19015cbe1e73b56351fa5c975
santerj <45292222+santerj@users.noreply.github.com>
2021-03-14 22:16:42 +0200

    moved notifier call to if statement

--- bin/login-police.sh
+++ bin/login-police.sh
@@ -6,6 +6,5 @@ date=$(date +"%a %F %T %Z")
 if [ "$PAM_TYPE" != "close_session" ]; then
     host="`hostname`"
     notification="New login: $PAM_USER from $PAM_RHOST on $host at $date"
+    /usr/local/bin/notifier.sh "$notification"
 fi
-
-/usr/local/bin/notifier.sh "$notification"
