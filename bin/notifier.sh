fcdd3f6803973ab8369401ae887bf2200ee6bba0
santerj <45292222+santerj@users.noreply.github.com>
2021-03-14 22:03:31 +0200

    dir curl output to /dev/null

--- bin/notifier.sh
+++ bin/notifier.sh
@@ -5,4 +5,4 @@ ID=$CHAT_ID
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 MESSAGE=$@
 
-curl -s -X POST $URL -d chat_id=$ID -d text="$@"
+curl -s -X POST $URL -d chat_id=$ID -d text="$@" > /dev/null 2>&1
