dc2a082f4e9612113c47fc9f5a4ca2147f044284
santerj <45292222+santerj@users.noreply.github.com>
2021-03-14 22:15:25 +0200

    add login police

---
+++ bin/login-police.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+host=$(hostname --fqdn)
+date=$(date +"%a %F %T %Z")
+
+if [ "$PAM_TYPE" != "close_session" ]; then
+    host="`hostname`"
+    notification="New login: $PAM_USER from $PAM_RHOST on $host at $date"
+fi
+
+/usr/local/bin/notifier.sh "$notification"
