4442b9c08041e86c3d4fb21c99a41dcbf27e41b5
santerj <45292222+santerj@users.noreply.github.com>
2025-10-07 22:08:42 +0300

    Update setup-fedora.sh

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -92,7 +92,7 @@ REQUIRED_REPOS=(
   rpmfusion-free
   rpmfusion-nonfree
   rpmfusion-nonfree-nvidia-driver
-  vscode
+  code
 )
 
 for repo in "${REQUIRED_REPOS[@]}"; do
@@ -113,7 +113,7 @@ for repo in "${REQUIRED_REPOS[@]}"; do
       rpmfusion-nonfree-nvidia-driver)
         dnf -y install rpmfusion-nonfree-nvidia-driver
         ;;
-      vscode)
+      code)
         rpm --import https://packages.microsoft.com/keys/microsoft.asc
         echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
         dnf -y install code
